acb068a3990c253e82d1401a0d6f1745248f609e
Shashank <shekharshashank093@gmail.com>
2024-02-09 04:23:42 +0530

    initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import "../Style/footer.css";
+const footer = () => {
+  return (
+    <>
+      <div className="footerdiv">
+        <div className="dev">
+          <span>Developer-Shashank_Shekhar</span>
+          <br />
+          <br />
+          <span>Location-Raipur,India</span>
+          <br />
+        </div>
+        <div className="end">
+          Copyright © www.KhaoPiyo.com all rights are reserved!!!
+        </div>
+        <div className="social">
+          <AiFillFacebook className="scoialImg" />
+          <span className="scoialWord">Facebook</span>
+          <br />
+          <AiFillLinkedin className="scoialImg" />
+          <span className="scoialWord">Linkedin</span>
+          <br />
+          <AiFillInstagram className="scoialImg" />
+          <span className="scoialWord">Instagram</span>
+          <br />
+        </div>
+      </div>
+    </>
+  );
+};
+export default footer;
